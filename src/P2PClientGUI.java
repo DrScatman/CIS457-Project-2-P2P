cec890ab4a48e7f5918353b2bbfc4f533b6204ab
MakerMom <finkbean@mail.gvsu.edu>
2019-11-10 19:38:22 -0500

    Made a file selection for the gui. "Connect" button toggles to "Disconnect" after pressing.

--- src/P2PClientGUI.java
+++ src/P2PClientGUI.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.io.File;
 import java.net.InetAddress;
 import java.util.*;
 import java.util.Arrays;
